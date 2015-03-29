65c385de8f635241a3ed4b697db6bf3f79f4ac7d
ednardomartins <ednardomartins@gmail.com>
2015-03-28 22:06:01 -0300

    Template openshift

--- src/main/java/com/ednardo/predador/model/mongo/MongoConnection.java
+++ src/main/java/com/ednardo/predador/model/mongo/MongoConnection.java
@@ -3,6 +3,7 @@ package com.ednardo.predador.model.mongo;
 import java.net.UnknownHostException;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
@@ -10,14 +11,17 @@ import com.mongodb.MongoClient;
 
 public class MongoConnection {
 
-	public static final String DB_NAME = "loteca";
+	public static final String DB_NAME = "boot";
 	public static final String MONGO_HOST = "localhost";
+	public static final String USER = "admin";
+	public static final String PASSWORD = "YrFjWBsNd3ij";
 	public static final int MONGO_PORT = 27017;
 
 	@Bean
 	public MongoOperations connection() throws UnknownHostException {
 		MongoClient mongo = new MongoClient(MONGO_HOST, MONGO_PORT);
-		MongoOperations mongoOps = new MongoTemplate(mongo, DB_NAME);
+		UserCredentials userCredentials = new UserCredentials(USER, PASSWORD);
+		MongoOperations mongoOps = new MongoTemplate(mongo, DB_NAME, userCredentials);
 		return mongoOps;
 	}
 }
